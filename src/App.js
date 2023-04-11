8de46b43a256b6f94f6e29cffb8c13f3b23d287f
YAChen123 <andychen123583@gmail.com>
2023-04-11 00:14:10 -0400

    Revert "Revert "frontend""
    This reverts commit 3eb93cc25efbed4a28d054dda837c4462c7782f6.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './main.scss';
+import Layout from './components/Layout.js';
+import { GlobalProvider } from './context/GlobalContext';
+
+function App() {
+  return ( 
+    <GlobalProvider>
+      <Layout />
+    </GlobalProvider>
+  );
+}
+
+export default App;
